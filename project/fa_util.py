3ad8084886759682e9db68f14f9293fefcce389c
mishavint <mishavint@mail.ru>
2022-09-16 18:58:06 +0300

    Code style fix

--- project/fa_util.py
+++ project/fa_util.py
@@ -5,57 +5,58 @@ from networkx import MultiGraph
 
 
 def create_minimum_dfa(regex: Regex) -> DeterministicFiniteAutomaton:
-	"""
-	Creates minimum deterministic finite automaton
-	Parameters
-	----------
-	regex : Regex
-		needed regex
+    """
+    Creates minimum deterministic finite automaton
+    Parameters
+    ----------
+    regex : Regex
+            needed regex
 
-	Returns
-	-------
-	DeterministicFiniteAutomaton
-		created dfa
-	"""
-	enfa = regex.to_epsilon_nfa()
+    Returns
+    -------
+    DeterministicFiniteAutomaton
+            created dfa
+    """
+    enfa = regex.to_epsilon_nfa()
 
-	return enfa.to_deterministic().minimize()
+    return enfa.to_deterministic().minimize()
 
 
-def create_ndfa_by_graph(graph: MultiGraph, start_states: Set[int] = None,
-						 final_states: Set[int] = None) -> NondeterministicFiniteAutomaton:
-	"""
-		Creates a NonDeterministic Finite Automation by graph
+def create_ndfa_by_graph(
+    graph: MultiGraph, start_states: Set[int] = None, final_states: Set[int] = None
+) -> NondeterministicFiniteAutomaton:
+    """
+            Creates a NonDeterministic Finite Automation by graph
 
-	Parameters
-	----------
-	graph: MultiGraph
-		needed graph
-	start_states: Set[int]
-		Set of start edges
-	final_states: Set[int]
-		Set of final edges
+    Parameters
+    ----------
+    graph: MultiGraph
+            needed graph
+    start_states: Set[int]
+            Set of start edges
+    final_states: Set[int]
+            Set of final edges
 
-	Returns
-	-------
-	NondeterministicFiniteAutomaton
-		NDFA
-	"""
-	ndfa = NondeterministicFiniteAutomaton()
-	for edge in graph.edges(data=True):
-		ndfa.add_transition(edge[0], edge[2]["label"], edge[1])
+    Returns
+    -------
+    NondeterministicFiniteAutomaton
+            NDFA
+    """
+    ndfa = NondeterministicFiniteAutomaton()
+    for edge in graph.edges(data=True):
+        ndfa.add_transition(edge[0], edge[2]["label"], edge[1])
 
-	if (start_states and final_states) is None:
-		for state in ndfa.states:
-			ndfa.add_start_state(state)
-			ndfa.add_final_state(state)
+    if (start_states and final_states) is None:
+        for state in ndfa.states:
+            ndfa.add_start_state(state)
+            ndfa.add_final_state(state)
 
-	if start_states:
-		for state in start_states:
-			ndfa.add_start_state(State(state))
+    if start_states:
+        for state in start_states:
+            ndfa.add_start_state(State(state))
 
-	if final_states:
-		for state in final_states:
-			ndfa.add_final_state(State(state))
+    if final_states:
+        for state in final_states:
+            ndfa.add_final_state(State(state))
 
-	return ndfa
+    return ndfa
--- tests/test_fa_util.py
+++ tests/test_fa_util.py
@@ -3,53 +3,55 @@ from project.graphs_util import *
 
 
 class TestsForCreateMinimumFaFunction:
-	def test_is_dfa(self):
-		dfa = create_minimum_dfa(Regex(""))
-		assert dfa.is_deterministic()
-		assert dfa.is_empty()
+    def test_is_dfa(self):
+        dfa = create_minimum_dfa(Regex(""))
+        assert dfa.is_deterministic()
+        assert dfa.is_empty()
 
-	def test_for_union(self):
-		dfa = create_minimum_dfa(Regex("a|c"))
-		assert dfa.accepts([Symbol("a")])
-		assert dfa.accepts([Symbol("c")])
-		assert not dfa.accepts([Symbol("b")])
-		assert not dfa.accepts([Symbol("a"), Symbol("c")])
+    def test_for_union(self):
+        dfa = create_minimum_dfa(Regex("a|c"))
+        assert dfa.accepts([Symbol("a")])
+        assert dfa.accepts([Symbol("c")])
+        assert not dfa.accepts([Symbol("b")])
+        assert not dfa.accepts([Symbol("a"), Symbol("c")])
 
-	def test_for_concatenation(self):
-		dfa = create_minimum_dfa(Regex("a c"))
-		assert dfa.accepts([Symbol("a"), Symbol("c")])
-		assert not dfa.accepts([Symbol("ac")])
-		assert not dfa.accepts([Symbol("a c")])
+    def test_for_concatenation(self):
+        dfa = create_minimum_dfa(Regex("a c"))
+        assert dfa.accepts([Symbol("a"), Symbol("c")])
+        assert not dfa.accepts([Symbol("ac")])
+        assert not dfa.accepts([Symbol("a c")])
 
-	def test_for_kleene(self):
-		dfa = create_minimum_dfa(Regex("a*"))
-		assert dfa.accepts([Symbol("a")])
-		assert dfa.accepts([Symbol("a"), Symbol("a")])
+    def test_for_kleene(self):
+        dfa = create_minimum_dfa(Regex("a*"))
+        assert dfa.accepts([Symbol("a")])
+        assert dfa.accepts([Symbol("a"), Symbol("a")])
 
-	def test_for_parentheses(self):
-		dfa = create_minimum_dfa(Regex("a (b|c)"))
-		assert dfa.accepts([Symbol("a"), Symbol("b")])
-		assert dfa.accepts([Symbol("a"), Symbol("c")])
-		assert not dfa.accepts([Symbol("a"), Symbol("bc")])
-		assert not dfa.accepts([Symbol("a"), Symbol("b"), Symbol("c")])
+    def test_for_parentheses(self):
+        dfa = create_minimum_dfa(Regex("a (b|c)"))
+        assert dfa.accepts([Symbol("a"), Symbol("b")])
+        assert dfa.accepts([Symbol("a"), Symbol("c")])
+        assert not dfa.accepts([Symbol("a"), Symbol("bc")])
+        assert not dfa.accepts([Symbol("a"), Symbol("b"), Symbol("c")])
 
 
 class TestsForCreateNdfaByGraph:
-	def test_is_ndfa(self):
-		graph_for_test = get_graph_by_name("generations")
-		assert not create_ndfa_by_graph(graph=graph_for_test).is_deterministic()
+    def test_is_ndfa(self):
+        graph_for_test = get_graph_by_name("generations")
+        assert not create_ndfa_by_graph(graph=graph_for_test).is_deterministic()
 
-	def test_works_as_expected(self):
-		two_cycles_graph = create_two_cycles_graph(1, 1, "a", "b")
-		actual_ndfa = create_ndfa_by_graph(two_cycles_graph, start_states={0}, final_states={2})
+    def test_works_as_expected(self):
+        two_cycles_graph = create_two_cycles_graph(1, 1, "a", "b")
+        actual_ndfa = create_ndfa_by_graph(
+            two_cycles_graph, start_states={0}, final_states={2}
+        )
 
-		expected_ndfa = NondeterministicFiniteAutomaton()
-		expected_ndfa.add_start_state(State(0))
-		expected_ndfa.add_final_state(State(2))
+        expected_ndfa = NondeterministicFiniteAutomaton()
+        expected_ndfa.add_start_state(State(0))
+        expected_ndfa.add_final_state(State(2))
 
-		expected_ndfa.add_transition(State(1), Symbol("a"), State(0))
-		expected_ndfa.add_transition(State(0), Symbol("a"), State(1))
-		expected_ndfa.add_transition(State(2), Symbol("b"), State(0))
-		expected_ndfa.add_transition(State(0), Symbol("b"), State(2))
+        expected_ndfa.add_transition(State(1), Symbol("a"), State(0))
+        expected_ndfa.add_transition(State(0), Symbol("a"), State(1))
+        expected_ndfa.add_transition(State(2), Symbol("b"), State(0))
+        expected_ndfa.add_transition(State(0), Symbol("b"), State(2))
 
-		assert actual_ndfa.is_equivalent_to(expected_ndfa)
+        assert actual_ndfa.is_equivalent_to(expected_ndfa)
