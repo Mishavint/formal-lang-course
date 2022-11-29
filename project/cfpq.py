01ebf82771a8cb9db6392cca956a84d09a2377ee
mishavint <mishavint@mail.ru>
2022-11-29 12:05:56 +0300

    11th task

--- project/cfpq.py
+++ project/cfpq.py
@@ -1,10 +1,14 @@
 from networkx import MultiDiGraph
 from pyformlang.cfg import CFG, Variable
+from pyformlang.finite_automaton import EpsilonNFA
 from enum import Enum
 from scipy import sparse
 import numpy
 
 from project.cfg_util import create_wcnf_by_cfg
+from project.rsm import RSM
+from project.ecfg import ECFG
+from project.bool_matrices import BoolMatrices
 
 
 def hellings_closure(cfg: CFG, graph: MultiDiGraph):
@@ -135,9 +139,54 @@ def matrix_closure(cfg: CFG, graph: MultiDiGraph):
     return res
 
 
+def tensor_closure(cfg: CFG, graph: MultiDiGraph):
+    ecfg = ECFG.from_cfg(cfg)
+    rsm = RSM.create_rsm_by_ecfg(ecfg)
+
+    rbm = BoolMatrices().create_by_rsm(rsm)
+    index_to_state = {i: st for st, i in rbm.states_indices.items()}
+
+    graph_bm = BoolMatrices(EpsilonNFA.from_networkx(graph))
+    graph_bm_size = len(graph_bm.states_indices)
+    graph_state_indicies = {i: st for st, i in graph_bm.states_indices.items()}
+    loop_m = sparse.eye(len(graph_bm.states_indices), dtype=bool).todok()
+
+    for nonterm in cfg.get_nullable_symbols():
+        graph_bm.bool_matrices[nonterm.value] += loop_m
+    old_sz = 0
+
+    while True:
+        intersection = rbm & graph_bm
+        tc_indices = list(zip(*intersection.transitive_closure().nonzero()))
+        if len(tc_indices) == old_sz:
+            break
+        old_sz = len(tc_indices)
+
+        for i, j in tc_indices:
+            cfg_i = i // graph_bm_size
+            cfg_j = j // graph_bm_size
+            graph_i = i % graph_bm_size
+            graph_j = j % graph_bm_size
+
+            first_state = index_to_state[cfg_i]
+            second_state = index_to_state[cfg_j]
+
+            nonterm, _ = first_state.value
+
+            if first_state in rbm.start_states and second_state in rbm.final_states:
+                graph_bm.bool_matrices[nonterm][graph_i, graph_j] = True
+
+    return {
+        (graph_state_indicies[graph_i], nonterm, graph_state_indicies[graph_j])
+        for nonterm, mtx in graph_bm.bool_matrices.items()
+        for graph_i, graph_j in zip(*mtx.nonzero())
+    }
+
+
 class CfpqAlgorithms(Enum):
     HELLINGS = hellings_closure
     MATRIX = matrix_closure
+    TENSOR = tensor_closure
 
 
 def cfpq(
